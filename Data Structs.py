be08d3710aa974a669b71520b2d7b4f4e84090d7
tarun <tarun@mathewj.com>
2020-11-29 16:09:20 -0800

    Learning Data Stucts

---
+++ Data Structs.py
@@ -0,0 +1,8 @@
+# Student = ["Grade","Student ID","Credits"]
+# Class = {"Jeff": Student, "Bob": Student}
+# print(Class)
+
+Student = {"grade" : 90, "name" : "jeff"}
+print(Student["grade"])
+# Class = [Student, Student]
+# print(Class)
